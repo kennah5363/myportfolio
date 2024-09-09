5c4c42fed03bc61594a172071e89e24d5b02f9e7
kennah5363 <107480303+kennah5363@users.noreply.github.com>
2024-09-09 13:06:04 +0300

    Add files via upload

--- Js.js
+++ Js.js
@@ -1,3 +1,16 @@
+
+const name = document.getElementById("name");
+// if (person != null) {
+//   name.innerHTML = "Hello " + person + " It's Me"
+// }
+var person = window.prompt("Enter Your Name");
+if (person != null) {
+  name.innerHTML = "Hello " + person + ", It's Me";
+} else {
+  window.alert("Please Refresh And Enter Your Name!");
+}
+
+
 var typed = new Typed(".multiple-text", {
   strings:["Frontend developer", "Hairdresser", "Beautician"],
   typeSpeed:100,
@@ -5,3 +18,5 @@ var typed = new Typed(".multiple-text", {
   backDelay:1000,
   loop:true
 })
+
+
