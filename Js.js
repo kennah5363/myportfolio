6c57bed886b9ff0ba5325bdd011296428beb51ea
kennah5363 <107480303+kennah5363@users.noreply.github.com>
2024-09-09 13:09:01 +0300

    Update Js.js

--- Js.js
+++ Js.js
@@ -1,9 +1,9 @@
-
+var person = window.prompt("Enter Your Name");
 const name = document.getElementById("name");
 // if (person != null) {
 //   name.innerHTML = "Hello " + person + " It's Me"
 // }
-var person = window.prompt("Enter Your Name");
+
 if (person != null) {
   name.innerHTML = "Hello " + person + ", It's Me";
 } else {
